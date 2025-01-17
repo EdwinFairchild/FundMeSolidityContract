9dd19c5202428ef256d821c42f4d664d4458d638
EdwinFairchild <edwin@edwinfairchild.com>
2025-01-16 23:59:56 -0600

    withdraw function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,15 +10,24 @@ contract FundMe{
     //keep tract of fnders
     address[] public funders;
     // map an address type to a uint256 type
-    mapping(address fuuder => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
     
 
     function fund()  payable public{
         // msg.value is an uin256 and it is automatically passed as first argument to getEthToUSDRate
         require(msg.value.getEthToUSDRate() >= minFundAmountUSD, "Minimum required is 5 USD");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
 
+    }
+
+    function wuthdraw() public {
+        for( uint256 funderIndex = 0 ; funderIndex < funders.length ; funderIndex++){
+            // reset the amount funded associated with the funder
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+
+        }
     }
 
 
